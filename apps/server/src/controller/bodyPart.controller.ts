fa733a5215ef0fc1d4d8a89ffe99cb8e89038b51
cweiqi27 <chanweiqi27@gmail.com>
2023-05-09 19:41:05 +0800

    refactor: error messages and status code

--- apps/server/src/controller/bodyPart.controller.ts
+++ apps/server/src/controller/bodyPart.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import type { CreateBodyPartInput } from "../schema/bodyPart.schema";
 import { createBodyPart } from "../service/bodyPart.service";
+import { isNativeError } from "util/types";
 
 export const createBodyPartHandler = async (
   req: Request<
@@ -13,7 +14,24 @@ export const createBodyPartHandler = async (
   try {
     const bodyPart = await createBodyPart(req.body);
     return res.json(bodyPart);
-  } catch (e: any) {
-    return res.status(400).send(e);
+  } catch (e) {
+    if (isNativeError(e) && e.message === "11000") {
+      return res
+        .status(409)
+        .send("Request consists of duplicate value on a unique field");
+    }
+    return res.status(500).send("Internal server error");
+  }
+};
+
+export const getBodyPartHandler = async (req: Request, res: Response) => {
+  try {
+    const bodyPart = await createBodyPart(req.body);
+    return res.json(bodyPart);
+  } catch (e) {
+    if (isNativeError(e) && e.message === "404") {
+      return res.status(404).send("Body part not found");
+    }
+    return res.status(500).send("Internal server error");
   }
 };
